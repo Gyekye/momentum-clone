c29b87bd1636e5cba9a68d400fa22e240bc05c39
Gyekye <mrgyekye740@gmail.com>
2021-11-19 20:36:40 +0000

    catching errors

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("working Perfectly!")
+
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=cars")
+    .then(response => response.json())
+    .then(data => {
+        const bgImageUrl = data.urls.regular;
+        document.body.style.backgroundImage = `url(${bgImageUrl})`;
+        document.getElementById("image-author").textContent = `By:${data.user.name}`
+
+    })
+    .catch(error=> console.log(error))
+
+
+navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position.coords)
+)
+
